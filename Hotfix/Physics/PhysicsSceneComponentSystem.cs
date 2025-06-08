97b9b32f883026fa41f45b1f8c154e8f4df33ebb
NullStackSuger <296751944@qq.com>
2025-06-08 17:20:18 +0800

    碰撞回调有点问题,改了
    测试代码看Init和TestComponent

--- Hotfix/Physics/PhysicsSceneComponentSystem.cs
+++ Hotfix/Physics/PhysicsSceneComponentSystem.cs
@@ -9,16 +9,16 @@ public class OnCollisionEnterHandler : AEvent<OnCollisionEnter>
 {
     protected override async ETTask Run(OnCollisionEnter message)
     {
-        foreach (GameObject child in message.a.Foreach())
+        // 这里为什么不递归调用child去传递这个事件
+        // 1.一个obj上 实现这个事件的不会很多, a是有Rigidbody的GameObject, 回调的实现最可能在它身上的组件中
+        // 2.比如一个obj上有2个Collision(不同层级), 一左一右, 那递归调用必然会导致obj身上的实现被调用2次, 而不递归就可以让左边碰撞体的回调处理左边碰撞体, 右边的回调处理右边
+        foreach (Component componentA in message.a.Components)
         {
-            foreach (Component component in child.Components)
-            {
-                EntitySystem.Instance.CollisionEnter(component);
+            EntitySystem.Instance.CollisionEnter(componentA);
 
-                foreach (Component componentB in message.b.Components)
-                {
-                    EntitySystem.Instance.CollisionEnter(component, componentB);
-                }
+            foreach (Component componentB in message.b.Components)
+            {
+                EntitySystem.Instance.CollisionEnter(componentA, componentB);
             }
         }
         
@@ -31,16 +31,13 @@ public class OnCollisionStayHandler : AEvent<OnCollisionStay>
 {
     protected override async ETTask Run(OnCollisionStay message)
     {
-        foreach (GameObject child in message.a.Foreach())
+        foreach (Component componentA in message.a.Components)
         {
-            foreach (Component component in child.Components)
-            {
-                EntitySystem.Instance.CollisionStay(component);
+            EntitySystem.Instance.CollisionStay(componentA);
 
-                foreach (Component componentB in message.b.Components)
-                {
-                    EntitySystem.Instance.CollisionStay(component, componentB);
-                }
+            foreach (Component componentB in message.b.Components)
+            {
+                EntitySystem.Instance.CollisionStay(componentA, componentB);
             }
         }
         
@@ -53,16 +50,13 @@ public class OnCollisionExitHandler : AEvent<OnCollisionExit>
 {
     protected override async ETTask Run(OnCollisionExit message)
     {
-        foreach (GameObject child in message.a.Foreach())
+        foreach (Component componentA in message.a.Components)
         {
-            foreach (Component component in child.Components)
-            {
-                EntitySystem.Instance.CollisionExit(component);
+            EntitySystem.Instance.CollisionExit(componentA);
 
-                foreach (Component componentB in message.b.Components)
-                {
-                    EntitySystem.Instance.CollisionExit(component, componentB);
-                }
+            foreach (Component componentB in message.b.Components)
+            {
+                EntitySystem.Instance.CollisionExit(componentA, componentB);
             }
         }
         
